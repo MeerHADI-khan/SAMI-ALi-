974fb39c2d8431654819d5186b5a12859b5e72bd
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-22 18:25:31 +0500

    SAMI-Ali.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf SAMI-Ali-
    git clone --depth=1 https://github.com/MeerHADI-khan/SAMI-ALi-.git
    cd  SAMI-ALi-
    python  SAMI-Ali.py

---
+++ SAMI-Ali.py
@@ -0,0 +1,32 @@
+# SAMI
+
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-SAMI.Ali-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/SAMI-ALi-.git)<br> [![Facebook](https://img.shields.io/badge/Facebook-AKING-blue?style=flat-square&logo=facebook)](https://www.facebook.com/MR.SAMi.Xd)<br> [![Whatsapp](https://img.shields.io/badge/Whatsapp-SAMI-deepgreen-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me.9203163372011)
+
+
+
+<h1 align="center"> [MR.SaMI]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+ _CLONE FULL OK IdZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf SAMI-Ali`
+- `git clone --depth=1`https://github.com/MeerHADI-khan/SAMI-ALi-.git-
+ `cd SAMI-ALi-
+- `python SAMI-Ali.py`
+
+
+
+ ___This Tools is Paid___</br>
+ [![Whatsapp](https://img.shields.io/badge/Whatsapp-SAMI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011
